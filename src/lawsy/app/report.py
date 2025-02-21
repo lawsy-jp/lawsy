82d347bf95b587888ab2dbefdd790ef47aa1fd02
colorbear184 <colorkumalava1@gmail.com>
2025-02-22 00:09:03 +0900

    news with right column

--- src/lawsy/app/report.py
+++ src/lawsy/app/report.py
@@ -22,56 +22,56 @@ def get_logo_path() -> Path:
 def create_report_page(report: Report):
     def page_func():
         dotenv.load_dotenv()
-        #col1, col2 = st.columns([85, 15])
-        #with col1:
-        css = (Path(__file__).parent / "styles" / "style.css").read_text()
-        st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
+        col1, col2 = st.columns([85, 15])
+        with col1:
+            css = (Path(__file__).parent / "styles" / "style.css").read_text()
+            st.markdown(f"<style>{css}</style>", unsafe_allow_html=True)
 
-        logo = get_logo_path()
-        logger.info("reproduce previous report")
-        if st.session_state.config_reasoning_details_display_enabled and report.messages is not None:
-            with st.expander("Reasoning Details"):
-                for message in report.messages:
-                    role = message["role"]
-                    content = message["content"]
-                    if role == "user":
-                        with st.chat_message(role):
-                            st.write(content)
-                    else:
-                        with st.chat_message(role, avatar=logo):
-                            st.write(content)
-        pos = report.report_content.find("## ")
-        assert pos >= 0
-        title_and_lead = report.report_content[:pos]
-        rest = report.report_content[pos:]
-        title, lead = title_and_lead.split("\n", 1)
-        # title
-        st.write(title)
-        st.write(lead)
-        draw_mindmap(report.mindmap)
-        tooltips = get_reference_tooltip_html(report.references)
-        rest = embed_tooltips(rest, tooltips)
-        st.write(rest, unsafe_allow_html=True)
-        st.markdown("## References")
-        for i, result in enumerate(report.references, start=1):
-            html = get_hiddenbox_ref_html(i, result)
-            st.markdown(html, unsafe_allow_html=True)
+            logo = get_logo_path()
+            logger.info("reproduce previous report")
+            if st.session_state.config_reasoning_details_display_enabled and report.messages is not None:
+                with st.expander("Reasoning Details"):
+                    for message in report.messages:
+                        role = message["role"]
+                        content = message["content"]
+                        if role == "user":
+                            with st.chat_message(role):
+                                st.write(content)
+                        else:
+                            with st.chat_message(role, avatar=logo):
+                                st.write(content)
+            pos = report.report_content.find("## ")
+            assert pos >= 0
+            title_and_lead = report.report_content[:pos]
+            rest = report.report_content[pos:]
+            title, lead = title_and_lead.split("\n", 1)
+            # title
+            st.write(title)
+            st.write(lead)
+            draw_mindmap(report.mindmap)
+            tooltips = get_reference_tooltip_html(report.references)
+            rest = embed_tooltips(rest, tooltips)
+            st.write(rest, unsafe_allow_html=True)
+            st.markdown("## References")
+            for i, result in enumerate(report.references, start=1):
+                html = get_hiddenbox_ref_html(i, result)
+                st.markdown(html, unsafe_allow_html=True)
 
-        #with col2:
-        st.markdown('<div class="news-title">News</div>', unsafe_allow_html=True)
-        if report.news is not None:
-            for i, article in enumerate(report.news):
-                st.markdown(
-                    f'<a href="{article["link"]}" target="_blank">'
-                    f'<img src="{article["image"]}" width="250"></a>',
-                    unsafe_allow_html=True,
-                )
-                st.markdown(
-                    f'<div class="news-field">' f'<a href="{article["link"]}">{article["title"]}</div>',
-                    unsafe_allow_html=True,
-                )
-        else:
-            st.markdown('<div class="news-field">No News...</div>', unsafe_allow_html=True)
-        return
+            with col2:
+                st.markdown('<div class="news-title">News</div>', unsafe_allow_html=True)
+                if report.news is not None:
+                    for i, article in enumerate(report.news):
+                        st.markdown(
+                            f'<a href="{article["link"]}" target="_blank">'
+                            f'<img src="{article["image"]}" width="250"></a>',
+                            unsafe_allow_html=True,
+                        )
+                        st.markdown(
+                            f'<div class="news-field">' f'<a href="{article["link"]}">{article["title"]}</div>',
+                            unsafe_allow_html=True,
+                        )
+                else:
+                    st.markdown('<div class="news-field">No News...</div>', unsafe_allow_html=True)
+                return
 
     return page_func
